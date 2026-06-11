983b8c7e380de456c4353c94d9386d291d8e2ed0
dharmawebservice <dharmawebservice@gmail.com>
2026-06-11 11:18:14 +0530

    Add Next.js sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://dharmawebservice.in",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ];
+}
